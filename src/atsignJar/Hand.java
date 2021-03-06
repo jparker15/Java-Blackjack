cf104c54f473c118b2ad0f98470326c3c13d01c6
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-06 14:21:43 -0500

    holds cards drawn from deck,returns total score and displays cards in hand

--- src/atsignJar/Hand.java
+++ src/atsignJar/Hand.java
@@ -1,2 +1,40 @@
-package atsignJar;public class Hand {
+package atsignJar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private List<Card> cardsHeld;
+    private int score = 0;
+
+
+    public Hand (){
+        cardsHeld = new ArrayList<>();
+    }
+
+    public void addCard(Card card){
+        cardsHeld.add(card);
+//        System.out.println(card.displayFace());
+        System.out.println(score);
+    }
+
+    public void displayHand(){
+        for (Card card:cardsHeld) {
+            System.out.println(card.displayFace());
+        }
+    }
+
+    public void getHandValue(){
+
+        for (Card card:cardsHeld){
+           if(card.getValue() > 10){
+               score += 10;
+           }else{
+               score += card.getValue();
+           }
+
+        }
+        System.out.println(score);
+    }
+
 }
