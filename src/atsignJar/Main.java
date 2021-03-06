319514f0ff28a152ff6a7f6366339ba166d3e891
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-06 14:17:28 -0500

    move class instances to Blackjack class

--- src/atsignJar/Main.java
+++ src/atsignJar/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Deck myDeck = new Deck();
-//        myDeck.shuffle();
-        myDeck.playAll();
-
+        new Blackjack();
 
     }
 }
