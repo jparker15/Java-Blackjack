8047615b6fd2125ed8b47d110d0462ec4454d600
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-05 20:45:05 -0500

    create list of Cards, set suit then values via nested for each,draw method returns removed card souts if list is empty, shuffle method, playAll to check if deck contains all cards

--- src/atsignJar/Deck.java
+++ src/atsignJar/Deck.java
@@ -1,2 +1,38 @@
-package atsignJar;public class Deck {
+package atsignJar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+    final private int[] VALUES = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    final private String[] SUITS = {"HEARTS", "DIAMONDS", "CLUBS", "SPADES"};
+
+    public Deck(){
+        cards = new ArrayList<>();
+
+        for (var suit: SUITS){
+            for (var value: VALUES){
+                cards.add(new Card(suit,value));
+            }
+        }
+    }
+
+    public Card draw(){
+        if(cards.isEmpty()){
+            System.out.println("Deck is Empty");
+        }
+        return cards.remove(cards.size() - 1);
+    }
+
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+    public void playAll(){
+        for (int i = 0; i <= 52;i++ ){
+            System.out.println(draw().displayFace());
+        }
+    }
 }
