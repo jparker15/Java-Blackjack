a5f543fbf7aafcd4775c2e186bfbe2520b29e18f
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-07 23:33:15 -0500

    welcome, startTurn methods

--- src/atsignJar/Blackjack.java
+++ src/atsignJar/Blackjack.java
@@ -2,16 +2,31 @@ package atsignJar;
 
 public class Blackjack {
 
+    public static void main(String[] args) {
+       new Blackjack();
+    }
+
     public Blackjack(){
         Deck myDeck = new Deck();
         Hand myHand = new Hand();
 
-        myDeck.shuffle();
-        myDeck.draw();
-        myHand.addCard(myDeck.draw());
-        myHand.addCard(myDeck.draw());
-        myHand.addCard(myDeck.draw());
-        myHand.displayHand();
-        myHand.getHandValue();
+//        myDeck.shuffle();
+////        myDeck.playAll();
+//
+////        myHand.addCard(myDeck.draw());
+////        myHand.addCard(myDeck.draw());
+//        myHand.addCard(myDeck.draw());
+//        myHand.addCard(myDeck.draw());
+////        myHand.displayHand();
+////        myHand.getHandValue();
+    }
+
+    public void welcome (){
+        System.out.println("Welcome to Black Jack!");
+    }
+
+    public void startTurn(){
+        welcome();
+
     }
 }
