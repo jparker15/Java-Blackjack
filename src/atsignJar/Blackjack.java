173bca452b62f8d6220301cc97bef6406a9e0d05
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-09 19:43:26 -0500

    update overall game logic

--- src/atsignJar/Blackjack.java
+++ src/atsignJar/Blackjack.java
@@ -1,7 +1,10 @@
 package atsignJar;
 
+import java.util.Scanner;
+
 public class Blackjack {
 
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
        new Blackjack();
     }
@@ -9,24 +12,50 @@ public class Blackjack {
     public Blackjack(){
         Deck myDeck = new Deck();
         Hand myHand = new Hand();
+        myDeck.shuffle();
+//        startTurn(myDeck);
+        myHand.addCard(myDeck.draw());
+        myHand.addCard(myDeck.draw());
+        myHand.addCard(myDeck.draw());
+        myHand.displayHand();
+        System.out.println("break");
+        myDeck.playAll();
 
-//        myDeck.shuffle();
-////        myDeck.playAll();
-//
-////        myHand.addCard(myDeck.draw());
-////        myHand.addCard(myDeck.draw());
-//        myHand.addCard(myDeck.draw());
-//        myHand.addCard(myDeck.draw());
-////        myHand.displayHand();
-////        myHand.getHandValue();
     }
 
     public void welcome (){
         System.out.println("Welcome to Black Jack!");
+
     }
 
-    public void startTurn(){
+    public void startTurn(Deck deck){
         welcome();
+//        deck.playAll();
+        Hand playerHand = new Hand();
+        Hand dealerHand = new Hand();
+        playerHand.startHand(deck.draw());
+           while (true){
+               System.out.println("Hit (1) or Stand (2)");
+               int hitOrStand = scanner.nextInt();
+               if(hitOrStand == 1  ){
+                   playerHand.addCard(deck.draw());
+                   playerHand.displayHand();
+                   if(playerHand.checkScore() > 21){
+                       break;
+                   }
+               }
+               else{
+//                   playerHand.displayHand();
+                   break;
+               }
+           }
+        dealerHand.startHand(deck.draw());
+
+        playerHand.checkScore();
+           dealerHand.checkScore();
+
+
+
 
     }
 }
