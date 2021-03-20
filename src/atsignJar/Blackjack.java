d9fccb585c2a27427311cfb137ae72c28c306151
Jeremy <jeremyeparker@yahoo.com>
2021-03-19 20:34:18 -0400

    revamp game start logic, add checkWinner method

--- src/atsignJar/Blackjack.java
+++ src/atsignJar/Blackjack.java
@@ -3,59 +3,70 @@ package atsignJar;
 import java.util.Scanner;
 
 public class Blackjack {
+    Deck deck;
+    Player player;
+    Dealer dealer;
 
     Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-       new Blackjack();
-    }
 
-    public Blackjack(){
-        Deck myDeck = new Deck();
-        Hand myHand = new Hand();
-        myDeck.shuffle();
-//        startTurn(myDeck);
-        myHand.addCard(myDeck.draw());
-        myHand.addCard(myDeck.draw());
-        myHand.addCard(myDeck.draw());
-        myHand.displayHand();
-        System.out.println("break");
-        myDeck.playAll();
+    public Blackjack(Deck deck, Player player, Dealer dealer){
+       this.deck = deck;
+       this.player = player;
+       this.dealer = dealer;
 
     }
 
-    public void welcome (){
+    public void start(){
         System.out.println("Welcome to Black Jack!");
 
+        deck.shuffle();
+
+        player.addCard(deck.draw());
+        player.addCard(deck.draw());
+        player.displayHand();
+
+
+        while(true) {
+            System.out.println("Hit (1) or Stand (2)");
+            int a = scanner.nextInt();
+            player.checkScore();
+
+            if (a != 1) {break; }
+
+            if(!player.checkScore()){
+                break;
+            }
+            else{
+                player.addCard(deck.draw());
+                player.displayHand();
+            }
+        }
+
+        dealer.addCard(deck.draw());
+        dealer.addCard(deck.draw());
+        dealer.displayHand();
+
+        while(true){
+            if(dealer.getScore() > 17){
+                break;
+            }else{
+                System.out.println("Dealer:");
+                dealer.addCard(deck.draw());
+                dealer.displayHand();
+                dealer.getScore();;
+            }
+        }
+
+        checkWinner();
     }
 
-    public void startTurn(Deck deck){
-        welcome();
-//        deck.playAll();
-        Hand playerHand = new Hand();
-        Hand dealerHand = new Hand();
-        playerHand.startHand(deck.draw());
-           while (true){
-               System.out.println("Hit (1) or Stand (2)");
-               int hitOrStand = scanner.nextInt();
-               if(hitOrStand == 1  ){
-                   playerHand.addCard(deck.draw());
-                   playerHand.displayHand();
-                   if(playerHand.checkScore() > 21){
-                       break;
-                   }
-               }
-               else{
-//                   playerHand.displayHand();
-                   break;
-               }
-           }
-        dealerHand.startHand(deck.draw());
-
-        playerHand.checkScore();
-           dealerHand.checkScore();
-
-
-
-
+    public void checkWinner(){
+        if(player.getScore() > dealer.getScore()){
+            System.out.println("Winner");
+        }else if(player.getScore() == dealer.getScore()){
+            System.out.println("Draw");
+        }else{
+            System.out.println("Lose");
+        }
     }
 }
