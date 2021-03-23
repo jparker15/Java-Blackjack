629049873bf767642afbbb26264d98b278489f63
Jeremy <jeremyeparker@yahoo.com>
2021-03-22 20:42:20 -0400

    clean up / add points field

--- src/atsignJar/Player.java
+++ src/atsignJar/Player.java
@@ -1,40 +1,13 @@
 package atsignJar;
 
-<<<<<<< HEAD
-public class Player extends Hand{
+public class Player extends Hand {
     String name;
     int points;
 
 
-    public Player(String name){
+    public Player(String name) {
 
         this.name = name;
         this.points = 100;
-=======
-import java.util.Scanner;
-
-public class Player {
-    private String name;
-    private Hand hand = new Hand();
-    private int points = 100;
-
-    public Player (String name, Hand hand){
-        this.name = name;
-        this.hand = hand;
->>>>>>> 84226105d9683a7a205d0a53e1944517e16dc0b3
     }
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
->>>>>>> 84226105d9683a7a205d0a53e1944517e16dc0b3
 }
