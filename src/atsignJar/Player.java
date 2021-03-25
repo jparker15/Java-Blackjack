5925bc6249295e4d23230d57bec8cf53b3790a7c
Jeremy <jeremyeparker@yahoo.com>
2021-03-24 21:31:55 -0400

    add setters/getters

--- src/atsignJar/Player.java
+++ src/atsignJar/Player.java
@@ -1,23 +1,41 @@
 package atsignJar;
 
-import java.util.Scanner;
-
 public class Player extends Hand {
-    String name;
-    int points;
+    private String name;
+    private int points;
+    private int bet;
 
 
-    public Player(String name) {
+//    public Player(String name, int points) {
+//        this.setName(name);
+//        this.setPoints(points);
+//    }
 
+    public Player(){
+
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
         this.name = name;
-
     }
 
-    public Player() {
-
+    public int getBet() {
+        return bet;
     }
 
-    public void setName(){
-
+    public void setBet(int bet) {
+        this.bet = bet;
     }
 }
