ed0bed7cdf78b2da511e4fc19f2a3bda1bc55a5d
Jeremy <jeremyeparker@yahoo.com>
2021-03-24 21:34:03 -0400

    add set hidden value

--- src/atsignJar/Card.java
+++ src/atsignJar/Card.java
@@ -7,18 +7,25 @@ public class Card {
     private int value;
     private boolean isHidden = false;
 
-    public Card (String suit, int value){
+    public Card (String suit, int value, boolean isHidden){
         this.suit = suit;
         this.value = value;
+        this.isHidden = false;
+
     }
 
     public int getValue() {
         return value;
     }
 
+    public void setHidden(boolean status){this.isHidden = status;}
+
     public String displayFace(){
         String output = "";
 
+        if(isHidden){
+            return "[Hidden]";
+        }
         switch (value){
             case 1:
                 output= "AC";
