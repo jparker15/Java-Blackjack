b9d2e36aa674a1634a8ad030f8f5c127d5b10fb4
Jeremy <jeremyeparker@yahoo.com>
2021-03-27 18:06:51 -0400

    add to overall player points amt if bet is won

--- src/atsignJar/Main.java
+++ src/atsignJar/Main.java
@@ -1,10 +1,13 @@
 package atsignJar;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        new Blackjack(new Deck(), new Player("Jar"), new Dealer()).start();
+
+        new Blackjack(new Deck(), new Player(), new Dealer()).start();
 
     }
 }
