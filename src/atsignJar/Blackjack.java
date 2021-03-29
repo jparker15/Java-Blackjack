de69b28c166ddaaec07296269a670d1e1b5c64d9
Jeremy <jeremyeparker@yahoo.com>
2021-03-29 17:58:17 -0400

    reflect changes to total amount of chips based on checkWinner/ add a loop method to replay game

--- src/atsignJar/Blackjack.java
+++ src/atsignJar/Blackjack.java
@@ -18,19 +18,42 @@ public class Blackjack {
 
     public void start(){
         System.out.println("Welcome to Black Jack!");
-
         System.out.println("Enter Player Name:");
+
         String playerName = scanner.next();
         player.setName(playerName);
-//        System.out.println("Enter number of chips (1 - 100,000)");
-//        int chips = scanner.nextInt();
-//        player.setPoints(chips);
 
+        System.out.println("Enter number of chips (1 - 100,000)");
+
+        int chips = scanner.nextInt();
+        player.setPoints(chips);
         deck.shuffle();
 
-        System.out.println("How much would you like to bet?");
-        int bet = scanner.nextInt();
+        while (true){
+            gameLoop();
+            checkWinner();
+            System.out.println("Would you like to play again?");
+            String answer = scanner.next();
+            if(!answer.toLowerCase().contains("y")){
+                break;
+            }
+        }
+
+
+    }
+
+    public void gameLoop (){
+        int bet;
+        while (true){
+            System.out.println("How much would you like to bet?");
+            bet = scanner.nextInt();
+            if(bet < player.getPoints()){break;
+            }
+            System.out.println("Please place a smaller bet");
+
+        }
         player.setBet(bet);
+
         player.addCard(deck.draw());
         player.addCard(deck.draw());
         System.out.println(player.getName() + "'s Hand:");
@@ -38,9 +61,9 @@ public class Blackjack {
         player.checkScore();
 
         dealer.addCard((deck.draw()));
-        //Hides a dealer card
-        dealer.getHand().get(0).setHidden(true);
         dealer.addCard(deck.draw());
+        //Hides a dealer card
+        dealer.getHand().get(1).setHidden(true);
         System.out.println(dealer.name + "'s Hand:");
         dealer.displayHand();
 
@@ -59,7 +82,7 @@ public class Blackjack {
         }
 
         while(true){
-            dealer.getHand().get(0).setHidden(false);
+            dealer.getHand().get(1).setHidden(false);
             if(dealer.getScore() > 17){
                 dealer.displayHand();
                 dealer.checkScore();
@@ -72,31 +95,38 @@ public class Blackjack {
             }
 
         }
-        System.out.println();
-        checkWinner();
     }
 
     public void checkWinner(){
+        int winBet = (player.getBet() * 2);
         // Player didn't bust && dealer score is lower
         if(player.getScore() <= 21 && player.getScore() > dealer.getScore()){
             System.out.println(player.getName() + " Won!");
-//            player.setPoints(player.getBet() * 2);
-            System.out.println(player.getBet() * 2);
+            System.out.println(winBet);
+            player.setPoints(player.getPoints() + winBet);
+            System.out.println(player.getPoints());
             return;
         }// Dealer bust and player did not bust
         if(dealer.getScore() > 21 && player.getScore() <= 21){
             System.out.println(player.getName() + " Won!");
-            System.out.println(player.getBet() * 2);
+            System.out.println(winBet);
+            player.setPoints(player.getPoints() + winBet);
+            System.out.println(player.getPoints());
+
             return;
         }// dealer and player score same
         else if(player.getScore() == dealer.getScore()){
             System.out.println("Draw");
             System.out.println("Bet returned: " + player.getBet());
+            System.out.println(player.getPoints());
+
             return;
         }else{
             System.out.println("-" + player.getBet());
             System.out.println("Lose");
+            player.setPoints(player.getPoints() - player.getBet());
         }
+        System.out.println(player.getPoints());
     }
 
 
